e26c92e3569dd0a0503c1e669ea50c2eabb6d0c0
Cecchetto, Riccardo <riccardo.cecchetto@oracle.com>
2026-09-19 17:53:07 +0200

    add pet API client

---
+++ src/clients/pet.client.ts
@@ -0,0 +1,26 @@
+import type { APIRequestContext, APIResponse } from '@playwright/test';
+import type { Pet } from '../models/pet';
+
+export class PetClient {
+  constructor(private readonly request: APIRequestContext) {}
+
+  createPet(pet: Pet): Promise<APIResponse> {
+    return this.request.post('pet', {
+      data: pet,
+    });
+  }
+
+  getPet(petId: number): Promise<APIResponse> {
+    return this.request.get(`pet/${petId}`);
+  }
+
+  updatePet(pet: Pet): Promise<APIResponse> {
+    return this.request.put('pet', {
+      data: pet,
+    });
+  }
+
+  deletePet(petId: number): Promise<APIResponse> {
+    return this.request.delete(`pet/${petId}`);
+  }
+}
